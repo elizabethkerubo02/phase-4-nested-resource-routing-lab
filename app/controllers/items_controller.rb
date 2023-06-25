425ca65f3bdaf272d86b9417bf791c89ba650aa2
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-06-25 17:57:31 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    items = Item.find(params[:id])
+    render json: items, include: :user
+  end
+
+  def create
+    # if params[user_id]
+    item = Item.create!(items_params)
+    render json:item, status: :created
+
+  end
+  
+
+  private
+  def items_params
+    params.permit(:name,:description,:price,:user_id)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
   end
 
 end
